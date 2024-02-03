de2772090b140caba475e627e86e96881c87fb61
Krishiv <114113474+krishivunity72@users.noreply.github.com>
2024-02-03 10:30:07 -0600

    attempt 50293

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed;
+    public float xBoundlow = -68.5f;
+    public float yBoundhigh = 182.5f;
+    public float xBoundhigh = 60.4f;
+    public float yBoundlow = -1.5f;
+
+
+    private Vector3 direction;
+
+    void Start()
+    {
+        SetRandomDirection();
+    }
+
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "xsides")
+        {
+           direction = new Vector3(-direction.x, direction.y, direction.z);
+
+        }
+        if (collision.gameObject.tag == "ysides")
+        {
+            direction = new Vector3(direction.x, -direction.y, direction.z);
+        }
+    
+    }
+
+    public void SetRandomDirection()
+    {
+        direction = new Vector3(
+            Random.Range(-1f, 1f),
+            Random.Range(-1f, 1f),
+            0f
+        ).normalized;
+    }
+}

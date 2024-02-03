6de45e917be04e21bbb574afcb5444cb455c0b3a
Krishiv <114113474+krishivunity72@users.noreply.github.com>
2024-02-03 10:14:09 -0600

    Added base files, hope it works this time

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    public GameObject title;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        title.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            title.SetActive(false);
+        }
+    }
+}
